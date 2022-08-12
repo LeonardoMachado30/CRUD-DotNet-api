9df8b2a8b03901ea1b92b1cf4695f4fdfffea110
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2022-08-12 16:30:55 -0300

    Criando api dotnet CRUD

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -41,7 +41,7 @@ namespace MeuTodo.Controllers
                 : Ok(todo);
         }
 
-        [HttpPost(template:"todos")]
+        [HttpPost(template: "todos")]
         public async Task<IActionResult> PostAsync([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model)
         {
             try
@@ -62,7 +62,57 @@ namespace MeuTodo.Controllers
                 await context.Todos.AddAsync(todo);
                 await context.SaveChangesAsync();
 
-                return Created(uri:$"v1/todos/{todo.Id}", todo);
+                return Created($"v1/todos/{todo.Id}", todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut(template: "todos/{id}")]
+        public async Task<IActionResult> PutAsync([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model, [FromRoute] int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var todo = await context.Todos.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (todo == null)
+                {
+                    return NotFound();
+                }
+
+                todo.Title = model.Title;
+
+                context.Todos.Update(todo);
+                await context.SaveChangesAsync();
+
+                return Ok(todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+
+            }
+        }
+
+        [HttpDelete(template: "todos/{id}")]
+        [HttpPut(template: "todos/{id}")]
+        public async Task<IActionResult> DeleteAsync([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var todo = await context.Todos.FirstOrDefaultAsync(x => x.Id == id);
+            try
+            {
+
+                context.Todos.Remove(todo);
+                await context.SaveChangesAsync();
+
+                return Ok();
             }
             catch (Exception e)
             {
